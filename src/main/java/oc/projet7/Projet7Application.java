d109d020c39deaf243df0dbd46eb72024c9e2c65
KaiKrk <kaiqiang@outlook.fr>
2020-03-12 20:21:09 +0100

    Initial commit

---
+++ src/main/java/oc/projet7/Projet7Application.java
@@ -0,0 +1,13 @@
+package oc.projet7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projet7Application.class, args);
+    }
+
+}
